f037f12f22563c3c3a0bddbb1f48b139a2f32444
Bryan Alvarado <bryan.alvarado@nixi1.com>
2023-10-11 02:33:32 +0200

    log port

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,9 @@
  */
 require('dotenv').config();
 
-import api from './api';
-
 const PORT = process.env.PORT || 8080;
+console.log('PORT: ', PORT);
+import api from './api';
 
 api.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
