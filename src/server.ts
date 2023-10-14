db9952bbc1156622d6c75bf2a056673360cc46e9
Bryan Alvarado <bryan.alvarado@nixi1.com>
2023-10-14 02:56:22 +0200

    fix server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,5 @@
-/**
- * Starts the application on the port specified.
- */
 require('dotenv').config();
-
 const PORT = process.env.PORT || 8080;
-console.log('PORT: ', PORT);
 import express from 'express';
 import cors from 'cors';
 import { authMiddleware } from './middlewares/authMiddleware';
@@ -16,30 +11,13 @@ import { createRateLimiter } from './api/apiRateLimiter';
 import { languageMiddleware } from './middlewares/languageMiddleware';
 import authSocial from './api/auth/authSocial';
 import setupSwaggerUI from './api/apiDocumentation';
-console.log('here in api index.ts');
-console.log('creating app');
+
 const app = express();
-console.log('created app');
-// Enables CORS
 app.use(cors({ origin: true }));
-console.log('Enable cors');
-// Initializes and adds the database middleware.
-console.log('databaseMiddleware');
 app.use(databaseMiddleware);
-
-// Sets the current language of the request
-console.log('languageMiddleware');
 app.use(languageMiddleware);
-
-// Configures the authentication middleware
-// to set the currentUser to the requests
-console.log('authMiddleware');
 app.use(authMiddleware);
-
-// Setup the Documentation
-console.log('setupSwaggerUI');
 setupSwaggerUI(app);
-
 // Default rate limiter
 const defaultRateLimiter = createRateLimiter({
   max: 500,
@@ -47,15 +25,7 @@ const defaultRateLimiter = createRateLimiter({
   message: 'errors.429',
 });
 app.use(defaultRateLimiter);
-
-// Enables Helmet, a set of tools to
-// increase security.
-console.log('helmet');
 app.use(helmet());
-
-// Parses the body of POST/PUT request
-// to JSON
-console.log('body parser');
 app.use(
   bodyParser.json({
     verify: function (req, res, buf) {
@@ -68,22 +38,15 @@ app.use(
     },
   }),
 );
-console.log('routes');
-
-// Configure the Entity routes
 const routes = express.Router();
-
-// Enable Passport for Social Sign-in
-console.log('passport for social sign in');
 authSocial(app, routes);
-console.log('requiring routes');
 function loadModule(moduleName) {
   return new Promise((resolve, reject) => {
     try {
       const module = require(`./api/${moduleName}`).default(
         routes,
       );
-      console.log('loaded module name: ', moduleName);
+
       resolve(module);
     } catch (error) {
       reject(error);
@@ -153,22 +116,13 @@ const modules = [
 Promise.all(
   modules.map((moduleName) => loadModule(moduleName)),
 )
-  .then((modules) => {
-    console.log('required all the routes');
-    // Loads the Tenant if the :tenantId param is passed
-    console.log('tenantMiddleware');
+  .then(() => {
     routes.param('tenantId', tenantMiddleware);
-    // Add the routes to the /api endpoint
-    console.log('add api routes');
     app.use('/api', routes);
-    console.log('export app');
-    // Resto de la configuración de tu aplicación Express
-    const server = app.listen(PORT, () => {
-      console.log(`Listening on ${PORT}`);
+    app.listen(PORT, () => {
+      console.log('Server is running on port', PORT);
     });
-    server.timeout = 1000 * 60 * 10; // 10 minutes
-    console.log('server.timeout: ', server.timeout);
   })
   .catch((error) => {
-    console.error('Error al cargar módulos:', error);
+    console.log('error: ', error);
   });
